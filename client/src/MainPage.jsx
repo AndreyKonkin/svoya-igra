c760b5267f0eca4c1b53f9f1e61432fcb3c6c2f6
Mishanya <mikhail.olianenko@mail.ru>
2022-10-14 10:46:40 +0300

    some files deleted

--- client/src/MainPage.jsx
+++ client/src/MainPage.jsx
@@ -1,39 +1,7 @@
-import React, { useContext } from 'react';
-import {
-  Card, CardContent, CardMedia, Container, Grid, Typography,
-} from '@mui/material';
-import { CafeContext } from './Context/CafeContext';
+import React from 'react';
 
 export default function MainPage() {
-  const { cafes } = useContext(CafeContext);
-
   return (
-    <Container sx={{ py: 8 }} maxWidth="md">
-      <Grid container spacing={4}>
-        {cafes?.map((card) => (
-          <Grid item key={card} xs={12} sm={6} md={4}>
-            <Card
-              sx={{
-                height: '100%', display: 'flex', flexDirection: 'column', backgroundColor: '#7CACBD',
-              }}
-            >
-              <CardMedia
-                component="img"
-                image={`${card.url}`}
-                alt="random"
-              />
-              <CardContent sx={{ flexGrow: 1 }}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {`${card.cafeName}`}
-                </Typography>
-                <Typography>
-                  {`${card.description}`}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <div>Main</div>
   );
 }

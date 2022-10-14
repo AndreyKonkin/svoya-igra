bcc3c24f7cc1a0763111a219836f68eb1db9cfb0
Mishanya <mikhail.olianenko@mail.ru>
2022-10-14 11:29:56 +0300

    Appbar and login

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 import './styles/style.css';
-import UserContextProvider from './Context/UserContext';
-import CafeContextProvider from './Context/CafeContext';
 
 axios.defaults.baseURL = process.env.REACT_APP_BASEURL;
 axios.defaults.withCredentials = true;
@@ -13,10 +13,8 @@ axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <UserContextProvider>
-      <CafeContextProvider>
-        <App />
-      </CafeContextProvider>
-    </UserContextProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 );
